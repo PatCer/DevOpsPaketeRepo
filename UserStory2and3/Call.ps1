104bcfb8ed28c18991b2a07905cb8928227cb554
PatrikCer <patrik.cerna@plan-b-gmbh.com>
2024-03-07 14:37:03 +0100

    added .ps1 files to make template calls easier

---
+++ UserStory2and3/Call.ps1
@@ -0,0 +1,32 @@
+param (
+    [string]$environment
+)
+
+function DeployToDev {
+    Write-Host "Deploying to Dev environment..."
+    az deployment group create --resource rg-patrik-cerna --template-file UserStory2and3\IoTMessageResourceTemplate.bicep --parameters UserStory2and3\parameters.dev.json --confirm-with-what-if
+}
+
+function DeployToTest {
+    Write-Host "Deploying to Test environment..."
+    az deployment group create --resource rg-patrik-cerna --template-file UserStory2and3\IoTMessageResourceTemplate.bicep --parameters UserStory2and3\parameters.test.json --confirm-with-what-if
+}
+
+function DeployToMain {
+    Write-Host "Deploying to Main environment..."
+    az deployment group create --resource rg-patrik-cerna --template-file UserStory2and3\IoTMessageResourceTemplate.bicep --parameters UserStory2and3\parameters.json --confirm-with-what-if
+}
+
+# Main script logic
+if ($environment -eq 'dev') {
+    DeployToDev
+}
+elseif ($environment -eq 'test') {
+    DeployToTest
+}
+elseif ($environment -eq 'main') {
+    DeployToMain
+}
+else {
+    Write-Host "Invalid environment parameter. Please use 'dev', 'test', or 'main'."
+}
